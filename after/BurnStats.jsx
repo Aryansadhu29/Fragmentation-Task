32e6f745f3f52cc3ff5fc157394d65348f60aff7
Aryan Sadhu <105599687+Aryansadhu29@users.noreply.github.com>
2024-03-23 14:12:57 +0530

    Add files via upload

---
+++ after/BurnStats.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BurnStats = ({ statsSupplies, allSupplies, walletChain, coinData }) => {
+  return (
+    <div>
+      {/* Your burn stats UI here */}
+    </div>
+  );
+};
+
+export default BurnStats;
---
+++ after/TransactionTable.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TransactionTable = ({ burnTransactions, coinData }) => {
+  return (
+    <div>
+      {/* Your transaction table UI here */}
+    </div>
+  );
+};
+
+export default TransactionTable;
